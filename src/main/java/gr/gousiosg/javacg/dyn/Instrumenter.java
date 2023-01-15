4af69f1e4ca74f8936a6f43b94cfe8d7b98bb41e
skychen <skychen1900@gmail.com>
2023-01-16 00:10:37 +0900

    fixed class not found error, when run at server

--- src/main/java/gr/gousiosg/javacg/dyn/Instrumenter.java
+++ src/main/java/gr/gousiosg/javacg/dyn/Instrumenter.java
@@ -39,143 +39,155 @@ import java.util.regex.Pattern;
 import java.util.regex.PatternSyntaxException;
 
 import javassist.CannotCompileException;
+import javassist.ClassClassPath;
 import javassist.ClassPool;
 import javassist.CtBehavior;
 import javassist.CtClass;
+import javassist.LoaderClassPath;
 import javassist.NotFoundException;
 
 public class Instrumenter implements ClassFileTransformer {
 
-    static List<Pattern> pkgIncl = new ArrayList<>();
-    static List<Pattern> pkgExcl = new ArrayList<>();
+	static List<Pattern> pkgIncl = new ArrayList<>();
+	static List<Pattern> pkgExcl = new ArrayList<>();
 
-    public static void premain(String argument, Instrumentation instrumentation) {
+	public static void premain(String argument, Instrumentation instrumentation) {
 
-        // incl=com.foo.*,gr.bar.foo;excl=com.bar.foo.*
+		// incl=com.foo.*,gr.bar.foo;excl=com.bar.foo.*
 
-        if (argument == null) {
-            err("Missing configuration argument");
-            return;
-        }
+		if (argument == null) {
+			err("Missing configuration argument");
+			return;
+		}
 
-        err("Argument is: " + argument);
+		err("Argument is: " + argument);
 
-        String[] tokens = argument.split(";");
+		String[] tokens = argument.split(";");
 
-        if (tokens.length < 1) {
-            err("Missing delimiter ;");
-            return;
-        }
+		if (tokens.length < 1) {
+			err("Missing delimiter ;");
+			return;
+		}
 
-        for (String token : tokens) {
-            String[] args = token.split("=");
-            if (args.length < 2) {
-                err("Missing argument delimiter =:" + token);
-                return;
-            }
+		for (String token : tokens) {
+			String[] args = token.split("=");
+			if (args.length < 2) {
+				err("Missing argument delimiter =:" + token);
+				return;
+			}
 
-            String argtype = args[0];
+			String argtype = args[0];
 
-            if (!argtype.equals("incl") && !argtype.equals("excl")) {
-                err("Wrong argument: " + argtype);
-                return;
-            }
+			if (!argtype.equals("incl") && !argtype.equals("excl")) {
+				err("Wrong argument: " + argtype);
+				return;
+			}
 
-            String[] patterns = args[1].split(",");
+			String[] patterns = args[1].split(",");
 
-            for (String pattern : patterns) {
-                Pattern p = null;
-                err("Compiling " + argtype + " pattern:" + pattern + "$");
-                try {
-                    p = Pattern.compile(pattern + "$");
-                } catch (PatternSyntaxException pse) {
-                    err("pattern: " + pattern + " not valid, ignoring");
-                }
-                if (argtype.equals("incl"))
-                    pkgIncl.add(p);
-                else
-                    pkgExcl.add(p);
-            }
-        }
+			for (String pattern : patterns) {
+				Pattern p = null;
+				err("Compiling " + argtype + " pattern:" + pattern + "$");
+				try {
+					p = Pattern.compile(pattern + "$");
+				} catch (PatternSyntaxException pse) {
+					err("pattern: " + pattern + " not valid, ignoring");
+				}
+				if (argtype.equals("incl"))
+					pkgIncl.add(p);
+				else
+					pkgExcl.add(p);
+			}
+		}
 
-        instrumentation.addTransformer(new Instrumenter());
-    }
+		instrumentation.addTransformer(new Instrumenter());
+	}
 
-    public byte[] transform(ClassLoader loader, String className, Class<?> clazz,
-            java.security.ProtectionDomain domain, byte[] bytes) {
-        boolean enhanceClass = false;
+	public byte[] transform(ClassLoader loader, String className, Class<?> clazz,
+			java.security.ProtectionDomain domain, byte[] bytes) {
+		boolean enhanceClass = false;
 
-        String name = className.replace("/", ".");
+		String name = className.replace("/", ".");
 
-        for (Pattern p : pkgIncl) {
-            Matcher m = p.matcher(name);
-            if (m.matches()) {
-                enhanceClass = true;
-                break;
-            }
-        }
+		for (Pattern p : pkgIncl) {
+			Matcher m = p.matcher(name);
+			if (m.matches()) {
+				enhanceClass = true;
+				break;
+			}
+		}
 
-        for (Pattern p : pkgExcl) {
-            Matcher m = p.matcher(name);
-            if (m.matches()) {
-                err("Skipping class: " + name);
-                enhanceClass = false;
-                break;
-            }
-        }
+		for (Pattern p : pkgExcl) {
+			Matcher m = p.matcher(name);
+			if (m.matches()) {
+				err("Skipping class: " + name);
+				enhanceClass = false;
+				break;
+			}
+		}
 
-        if (enhanceClass) {
-            return enhanceClass(className, bytes);
-        } else {
-            return bytes;
-        }
-    }
+		if (enhanceClass) {
+			return enhanceClass(className, bytes);
+		} else {
+			return bytes;
+		}
+	}
 
-    private byte[] enhanceClass(String name, byte[] b) {
-        ClassPool pool = ClassPool.getDefault();
-        CtClass clazz = null;
-        try {
-            clazz = pool.makeClass(new ByteArrayInputStream(b));
-            if (!clazz.isInterface()) {
-                err("Enhancing class: " + name);
-                CtBehavior[] methods = clazz.getDeclaredBehaviors();
-                for (int i = 0; i < methods.length; i++) {
-                    if (!methods[i].isEmpty()) {
-                        enhanceMethod(methods[i], clazz.getName());
-                    }
-                }
-                b = clazz.toBytecode();
-            }
-        } catch (CannotCompileException e) {
-            e.printStackTrace();
-            err("Cannot compile: " + e.getMessage());
-        } catch (NotFoundException e) {
-            e.printStackTrace();
-            err("Cannot find: " + e.getMessage());
-        } catch (IOException e) {
-            err("Error writing: " + e.getMessage());
-        } finally {
-            if (clazz != null) {
-                clazz.detach();
-            }
-        }
-        return b;
-    }
+	private byte[] enhanceClass(String name, byte[] b) {
+		//		ClassPool pool = ClassPool.getDefault();
 
-    private void enhanceMethod(CtBehavior method, String className)
-            throws NotFoundException, CannotCompileException {
-        String name = className.substring(className.lastIndexOf('.') + 1, className.length());
-        String methodName = method.getName();
+		// fixed class not found error, when run at server 
+		ClassPool parent = ClassPool.getDefault();
+		ClassPool cp = new ClassPool(parent);
+		cp.insertClassPath(new ClassClassPath(this.getClass()));
+		ClassLoader cl = Thread.currentThread()
+				.getContextClassLoader();
+		cp.insertClassPath(new LoaderClassPath(cl));
 
-        if (method.getName().equals(name))
-            methodName = "<init>";
+		CtClass clazz = null;
+		try {
+			clazz = cp.makeClass(new ByteArrayInputStream(b));
+			if (!clazz.isInterface()) {
+				err("Enhancing class: " + name);
+				CtBehavior[] methods = clazz.getDeclaredBehaviors();
+				for (int i = 0; i < methods.length; i++) {
+					if (!methods[i].isEmpty()) {
+						enhanceMethod(methods[i], clazz.getName());
+					}
+				}
+				b = clazz.toBytecode();
+			}
+		} catch (CannotCompileException e) {
+			e.printStackTrace();
+			err("Cannot compile: " + e.getMessage());
+		} catch (NotFoundException e) {
+			e.printStackTrace();
+			err("Cannot find: " + e.getMessage());
+		} catch (IOException e) {
+			err("Error writing: " + e.getMessage());
+		} finally {
+			if (clazz != null) {
+				clazz.detach();
+			}
+		}
+		return b;
+	}
 
-        method.insertBefore("gr.gousiosg.javacg.dyn.MethodStack.push(\"" + className
-                + ":" + methodName + "\");");
-        method.insertAfter("gr.gousiosg.javacg.dyn.MethodStack.pop();");
-    }
+	private void enhanceMethod(CtBehavior method, String className)
+			throws NotFoundException, CannotCompileException {
+		String name = className.substring(className.lastIndexOf('.') + 1, className.length());
+		String methodName = method.getName();
 
-    private static void err(String msg) {
-        //System.err.println("[JAVACG-DYN] " + msg);
-    }
+		if (method.getName()
+				.equals(name))
+			methodName = "<init>";
+
+		method.insertBefore("gr.gousiosg.javacg.dyn.MethodStack.push(\"" + className
+				+ ":" + methodName + "\");");
+		method.insertAfter("gr.gousiosg.javacg.dyn.MethodStack.pop();");
+	}
+
+	private static void err(String msg) {
+		System.err.println("[JAVACG-DYN] " + msg);
+	}
 }
